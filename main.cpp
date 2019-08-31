8c4d4fb3ab546295d3229f0e9bb7bf4f3c1dc554
JohnDinh2 <johndinh201@gmail.com>
2019-08-31 14:44:00 -0700

    Expenses program made

--- main.cpp
+++ main.cpp
@@ -1,2 +1,40 @@
-// Name:
+// Name: John Dinh
 // This program calculates and displays business expenses.
+#include <iostream>
+#include <string>
+int main()
+{
+  std::string location;
+
+  int days;
+
+  double hotelCost;
+
+  double foodCost;
+
+  double totalCost;
+
+  std::cout << "You\'re on a trip. Where?"<< std::endl;//Get location
+
+  std::cin >> location;
+
+  std::cout << "How long is the stay?" << std::endl;//Get days
+
+  std::cin >> days;
+
+  std::cout << "How much was the meal?" << '\n';//Find cost for food
+
+  std::cin >> foodCost;
+
+  std::cout << "How much was the hotel?" << '\n';// Find cost for hotel
+
+  std::cin >> hotelCost;
+
+  totalCost = hotelCost + foodCost;// find total cost by combining expenses for hotel and food
+
+  std::cout << "Location\t" << "Days\t" << "Hotel\t" << "Meal\t" << "Total\t" << std::endl;//display results afterwards
+
+  std:: cout << location << "\t" << "\t" << days << "\t" << "$" << hotelCost << "\t" << "$" << foodCost << "\t" << "$" << totalCost<< std::endl;
+
+  return 0;
+}
